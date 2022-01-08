87ba5b978422392f9a5c99b17a4a23216da24d01
A-S620 <aatikaseedat00@gmail.com>
2022-01-08 14:11:29 +0000

    Mocha setup

---
+++ tests/Test.tsx
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe('Options tests', () => {
+    it('checking default options', () => {
+        const meep = 5;
+        expect(meep).to.equal(5)
+    });
+});
